5db9be23b6a827f5fafd8f84d0baf4b05c502467
ell <ctorek@clemson.edu>
2024-12-26 02:32:20 +0000

    day 14 part 2

--- day14.rb
+++ day14.rb
@@ -7,34 +7,42 @@ input = IO.readlines("inputs/14", chomp: true).map do |s|
 end
 
 # part 1
-x = 101
-y = 103
-finalPos = input.map do |p| 
-  f = p[0] + 100 * p[1]
-  f[0] = f[0] % x
-  f[1] = f[1] % y
-  f
+$x = 101
+$y = 103
+
+def state(s, t)
+  s.map do |p|
+    f = p[0] + t * p[1]
+    f[0] = f[0] % $x
+    f[1] = f[1] % $y
+    f
+  end
 end
 
-p [:<, :>].repeated_permutation(2).map { |o| 
-  finalPos.count { |p| p[0].send(o[0], (x-1)/2) and p[1].send(o[1], (y-1)/2) }
-}.inject(:*)
+def score(s, t)
+  result = state(s, t)
+  [:<, :>].repeated_permutation(2).map { |o|
+    result.count { |p| p[0].send(o[0], ($x-1)/2) and p[1].send(o[1], ($y-1)/2) }
+  }.inject(:*)
+end
+
+def printState(s, t)
+  result = state(s, t)
+  for i in 0..$x
+    for j in 0..$y
+      if result.include? Vector[i, j]
+        print "#"
+      else
+        print "."
+      end
+    end
+    puts ""
+  end
+end
+
+p score(input, 100)
 
 # part 2
-def state(s, n) 
-  s.map do |p|
-    f = p[0] + n * p[1]
-    f[0] = f[0] % x
-    f[1] = f[1] % y
-  end
-end
-
-def printState(a)
-  x.times do |i|
-    y.times do |j|
-      if a.include?(Vector[i, j]) then puts "#" else puts "." end
-    end
-  end
-end
-
-(0..10).each { |i| printState(state(input, i)) }
+scores = (0..20000).map { |t| score(input, t) }
+p scores.index scores.min
+printState(input, scores.index(scores.min))
